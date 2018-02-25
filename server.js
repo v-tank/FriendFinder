e88ce12ae1bce93a42e6d7351fecb25753bc95ef
Vaibhav Tank <vaibhav@Macbook-Pro.local>
2018-02-24 21:54:09 -0800

    added express.static method to use custom css

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var bodyParser = require("body-parser");
 // Tells node that we are creating an "express" server
 var app = express();
 
+app.use(express.static('./app/public'))
+
 // Sets an initial port. We"ll use this later in our listener
 var PORT = process.env.PORT || 8080;
 
