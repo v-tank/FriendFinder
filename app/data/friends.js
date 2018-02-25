a5ddf4a72136a6925934a98a8f92b097a97e1787
Vaibhav Tank <vaibhav@Macbook-Pro.local>
2018-02-24 21:52:31 -0800

    seed data

---
+++ app/data/friends.js
@@ -0,0 +1,19 @@
+var friendsArray = [
+  {
+    "name": "Vaibhav",
+    "photo": "https://github.com/v-tank/Find-Your-Nature/blob/master/assets/images/vaibhav.png",
+    "scores": [1, 4, 2, 3, 5, 4, 3, 5, 5, 2] 
+  },
+  {
+    "name": "Tom",
+    "photo": "https://the-hollywood-gossip-res.cloudinary.com/iu/s--F_0FMLvw--/t_xlarge_p/cs_srgb,f_auto,fl_strip_profile.lossy,q_auto:420/v1517600897/tom-cruise-in-all-black.jpg",
+    "scores": [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+  },
+  {
+    "name": "Leo",
+    "photo": "http://cdn.realscreen.com/wp/wp-content/uploads/2018/01/Leo-DiCaprio.jpg?e8d71a",
+    "scores": [3, 4, 1, 2, 5, 1, 1, 5, 4, 5]
+  }
+];
+
+module.exports = friendsArray;
