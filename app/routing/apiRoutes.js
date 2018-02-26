273c17a1d853a3e4d559cda9d36e2de00179ed37
Vaibhav Tank <vaibhav@Macbook-Pro.local>
2018-02-25 22:25:58 -0800

    successfully calculates matches

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -13,12 +13,57 @@ module.exports = function(app) {
     var newUser = req.body;
     // newcharacter.routeName = newcharacter.name.replace(/\s+/g, "").toLowerCase();
 
-    // console.log(newUser);
+    console.log(newUser);
 
+    // Call function here!!!!!!!!!
+    var returnedResult = calculateCompatibility(newUser, friendsData);
+    var name = friendsData[returnedResult[0]].name;
+    var photoLink = friendsData[returnedResult[0]].photo;
+    // console.log("The closest match is " + name + "; he looks pretty handsome! " + photoLink);
+    
     friendsData.push(newUser);
 
-    console.log(friendsData);
+    returnedResult = {name: name, 
+      photo: photoLink};
+    // console.log(friendsData);
 
-    res.json(newUser);
+    res.json(returnedResult);
   });
+
+  function calculateCompatibility(newUser, friendsData) {
+    var calculatedScores = [];
+    for (var i = 0; i < friendsData.length; i++) {
+      var difference = 0;
+      for (var j = 0; j < friendsData[i].scores.length; j++) {
+        newUser.scores[j] = parseInt(newUser.scores[j]);
+        var jthDifference = Math.abs(newUser.scores[j] - friendsData[i].scores[j]);
+        difference += jthDifference;
+        // console.log(friendsData[i].scores[j] + "; " + jthDifference);
+      }
+      // console.log("The difference was: " + difference + " for element " + i);
+      calculatedScores.push({
+        difference: difference, 
+        index: i});
+    }
+    // console.log("\nHere are the scores and the indices:");
+    // console.log(calculatedScores);
+    // console.log("\n");
+    return findClosestMatch(newUser, calculatedScores);
+  }
+
+  function findClosestMatch(newUser, calculatedScores) {
+    var min = 0;
+    var index = 0;
+    for (var i = 0; i < calculatedScores.length; i++) {
+      if (i === 0) {
+        min = calculatedScores[i].difference;
+      }
+      else if (calculatedScores[i].difference < min) {
+        min = calculatedScores[i].difference;
+        index = i;
+      }
+    }
+    return [index, min];
+  }
+
 }
