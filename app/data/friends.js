db12bdd4c4923ba43ff5724d7c57897df2bed537
Vaibhav Tank <vaibhav@Macbook-Pro.local>
2018-02-26 23:11:28 -0800

    changed data format and added more friends

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,17 +2,37 @@ var friendsArray = [
   {
     "name": "Vaibhav",
     "photo": "https://v-tank.github.io/img/prof-pic.png",
-    "scores": [1, 4, 2, 3, 5, 4, 3, 5, 5, 2] 
+    "sex": "Male",
+    "interestedIn": "Women",
+    "scores": [4, 4, 3, 4, 3, 2, 3, 4, 2, 4] 
   },
   {
     "name": "Tom",
-    "photo": "https://the-hollywood-gossip-res.cloudinary.com/iu/s--F_0FMLvw--/t_xlarge_p/cs_srgb,f_auto,fl_strip_profile.lossy,q_auto:420/v1517600897/tom-cruise-in-all-black.jpg",
-    "scores": [5, 1, 4, 4, 3, 1, 2, 5, 4, 1]
+    "photo": "http://www.chicmags.com/wp-content/uploads/2013/12/tom-cruise-oblivion.jpg",
+    "sex": "Male",
+    "interestedIn": "Women",
+    "scores": [2, 4, 3, 1, 5, 4, 1, 2, 4, 5]
   },
   {
     "name": "Leo",
-    "photo": "http://cdn.realscreen.com/wp/wp-content/uploads/2018/01/Leo-DiCaprio.jpg?e8d71a",
-    "scores": [3, 4, 1, 2, 5, 1, 1, 5, 4, 5]
+    "photo": "http://redonline.cdnds.net/main/thumbs/23103/leonardodicapriosmile__square.jpg",
+    "sex": "Male",
+    "interestedIn": "Women",
+    "scores": [3, 2, 4, 3, 2, 1, 3, 1, 4, 5]
+  },
+  {
+    "name": "Gwyneth",
+    "photo": "http://redonline.cdnds.net/main/thumbs/22632/gwyneth-2__square.jpg",
+    "sex": "Female",
+    "interestedIn": "Men",
+    "scores": [2, 5, 3, 5, 5, 3, 2, 5, 2, 3]
+  },
+  {
+    "name": "Jennifer",
+    "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0annt2NpxfMK9iR4fE-O-ktljA7PDcI1U9uZj5B5JrRvyutiL3g",
+    "sex": "Female",
+    "interestedIn": "Men",
+    "scores": [4, 5, 2, 3, 2, 3, 1, 3, 2, 5]
   }
 ];
 
