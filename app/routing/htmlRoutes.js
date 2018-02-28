a0f2cf43a034c7113fa638137fd8f90997c42e66
Vaibhav Tank <vaibhav@Macbook-Pro.local>
2018-02-27 23:29:00 -0800

    added comments

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -3,10 +3,12 @@ var path = require("path");
 
 module.exports = function(app) {
   app.get("/survey", function(req, res) {
+    // serves the survey file
     res.sendFile(path.join(__dirname, "../public/survey.html"));
   });
 
   app.get("*", function(req, res) {
+    // catch-all to re-direct any other url to the homepage
     res.sendFile(path.join(__dirname, "../public/home.html"));
   });
 }
